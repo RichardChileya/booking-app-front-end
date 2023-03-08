6acf532e6fad0a8eac890c56fa17d3c548544569
tashisrael <tashisrael04@gmail.com>
2023-03-08 13:01:53 +0300

    add login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => (
-  <div>Login</div>
+  <div className="container mt-5">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <div className="card">
+          <div className="card-header">
+            Login
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="emailInput">Email address</label>
+                <input type="email" className="form-control" id="emailInput" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="passwordInput">Password</label>
+                <input type="password" className="form-control" id="passwordInput" />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Login;
