0b24b3d2258879e3a6e6940330aceb0d152d0935
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-03-09 09:55:55 +0100

    Update Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import HomeComponent from '../components/HomeComponent';
-import Header from '../components/Header';
 
-const Home = () => (
-  <>
-    <Header />
-    <HomeComponent />
-  </>
-);
+const Home = () => {
+  const logo = [
+    {
+      id: 1, name: 'Luxury Cars', image: 'https://t4.ftcdn.net/jpg/03/58/16/05/360_F_358160523_d0Tc3knJ0j1RBobgWCjM4BZUo9gx33ZX.jpg',
+    },
+  ];
+  return (
+    <div>
+      {logo.map((logoData) => (
+        <div key={logoData.id} className="booking-header">
+          <h1>
+            Welecome to
+            {' '}
+            {logoData.name}
+          </h1>
+          <img src={logoData.image} alt="" />
+        </div>
+
+      ))}
+      <HomeComponent />
+    </div>
+  );
+};
 
 export default Home;
