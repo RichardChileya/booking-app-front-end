b498ba51bdba82a9968cdbbd4b4f8a0faf0a16cc
recillah khamala <recillahk@gmail.com>
2023-03-10 17:01:56 +0300

    create the configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,2 +1,16 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import logg
+import logger from "redux-logger";
+import userSlice from "./reducer/userAuth/userSlice";
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+const store = configureStore(
+  {
+    reducer: rootReducer,
+    middleware: [...getDefaultMiddleware(), logger],
+  },
+);
+
+export default store
