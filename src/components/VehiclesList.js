3c7f1bcfe0a20c59b1255c13dd88f2e661fb0e71
RichardChileya <richardchileyawk@gmail.com>
2023-03-16 18:57:25 +0200

    Fix errors

--- src/components/VehiclesList.js
+++ src/components/VehiclesList.js
@@ -4,11 +4,9 @@ import Popup from './PopUp';
 
 const VehiclesList = ({ item }) => {
   const [showPopup, setShowPopup] = useState(false);
-  // const [selectedCar, setSelectedCar] = useState(null);
 
   const handleOpenPopup = () => {
     setShowPopup(true);
-    // setSelectedCar(car);
   };
 
   const handleClosePopup = () => {
@@ -25,16 +23,6 @@ const VehiclesList = ({ item }) => {
           <span><img src="https://cdn-icons-png.flaticon.com/512/356/356076.png" alt="twitter icon" className="icons" /></span>
           <span><img src="https://cdn-icons-png.flaticon.com/512/1362/1362857.png" alt="instagram icon" className="icons" /></span>
         </div>
-        {/* {showPopup && (
-        <div className="modal">
-          <div className="modal-content">
-            <button type="button" className="close" onClick={handleClosePopup}>&times;</button>
-            <h2>{item.name}</h2>
-            <p>{item.details}</p>
-            <img src={item.image} alt="modal vehicle" />
-          </div>
-        </div>
-        )} */}
       </div>
       <Popup showPopup={showPopup} handleClosePopup={handleClosePopup} item={item} />
     </>
