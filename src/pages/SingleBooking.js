e16f49da97373f384565349645aba67476447ed9
tashisrael <tashisrael04@gmail.com>
2023-03-17 21:07:44 +0300

    add singlebooking functionality

--- src/pages/SingleBooking.js
+++ src/pages/SingleBooking.js
@@ -5,15 +5,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import dayjs from 'dayjs';
 import { DatePicker } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { bookVehicle } from '../redux/reducer/bookings/bookingSlice';
 import { authenticatedUser } from '../redux/reducer/user/userSlice';
-import { vehicles, listVehicles } from '../redux/reducer/vehicles/vehicleSlice';
+import { vehicle, vehicleDetails } from '../redux/reducer/vehicles/vehicleSlice';
 
 const SingleBooking = () => {
   const [pickupDate, setPickupDate] = useState(null);
   const [returnDate, setReturnDate] = useState(null);
-  const allVehicles = useSelector(vehicles);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const allVehicles = useSelector(vehicle);
   const auth = useSelector(authenticatedUser);
   const [vehicleId, setVehicleId] = useState('');
   const dispatch = useDispatch();
@@ -24,7 +26,12 @@ const SingleBooking = () => {
   const handleVehicleId = (event) => setVehicleId(event.target.value);
 
   useEffect(() => {
-    dispatch(listVehicles());
+    const updateVehicle = async () => {
+      setLoading(true);
+      await dispatch(vehicleDetails(id));
+    };
+    updateVehicle();
+    setLoading(false);
   }, []);
 
   const handleBooking = async () => {
@@ -47,70 +54,70 @@ const SingleBooking = () => {
 
   document.title = 'Luxury Vehicles | Booking';
   return (
-    <>
-      <div className="mt-4 mx-auto w-100 bg-light border-0 rounded-3 p-3" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
-        <div className="mb-3 d-flex justify-content-center align-items-center text-light bg-dark opacity-50 bg-gradient" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
-          <h3 className="text-uppercase text-light" style={{ letterSpacing: '0.1em; font-weight: 300' }}>
-            Book a Vehicle
-          </h3>
-        </div>
-        <div className="text-center">
-          <h5 className="mb-2">Kind Note</h5>
-          <p className="text-left">
-            From Lamborghini to Mercedes, we have it all. Luxury Cars provides you with
-            the best luxury car rental services worldwide.
-          </p>
-        </div>
-        <div className="d-flex flex-column gap-4 text-danger">
-          <DatePicker
-            placeholder="Pickup Date"
-            placement="topLeft"
-            size="large"
-            format="YYYY/MM/DD"
-            allowClear
-            minDate={new Date()}
-            onChange={(date) => setPickupDate(handleDateFormat(date))}
-          />
-          <DatePicker
-            placeholder="Return Date"
-            placement="topLeft"
-            size="large"
-            format="YYYY/MM/DD"
-            allowClear
-            minDate={new Date()}
-            onChange={(date) => setReturnDate(handleDateFormat(date))}
-          />
-          <select
-            className=""
-            style={{ color: 'rgb(255, 193, 7)' }}
-            name="Vehicle"
-            defaultValue=""
-            label="Select a Vehicle"
-            onChange={handleVehicleId}
-            required
-          >
-            <option value="" disabled>Select a Vehicle</option>
-            {allVehicles.map((vehicleId) => (
-              <option value={vehicleId.id} key={vehicleId.id}>
-                {vehicleId.name}
-              </option>
-            ))}
-
-          </select>
-        </div>
-        <div className="mt-3">
-          <Button
-            type="button"
-            onClick={handleBooking}
-            variant="gradient"
-            className="text-uppercase font-weight-bold py-3 btn btn-primary w-100"
-            style={{ color: 'rgb(255, 193, 7)' }}
-          >
-            <span>Add Vehicle</span>
-          </Button>
-        </div>
-      </div>
-    </>
+    loading ? 'Loading'
+      : (
+        <>
+          <div className="mt-4 mx-auto w-100 bg-light border-0 rounded-3 p-3" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
+            <div className="mb-3 d-flex justify-content-center align-items-center text-light bg-dark opacity-50 bg-gradient" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
+              <h3 className="text-uppercase text-light" style={{ letterSpacing: '0.1em; font-weight: 300' }}>
+                Book This Vehicle
+              </h3>
+            </div>
+            <div className="text-center">
+              <h5 className="mb-2">Kind Note</h5>
+              <p className="text-left">
+                From Lamborghini to Mercedes, we have it all. Luxury Cars provides you with
+                the best luxury car rental services worldwide.
+              </p>
+            </div>
+            <div className="d-flex flex-column gap-4 text-danger">
+              <DatePicker
+                placeholder="Pickup Date"
+                placement="topLeft"
+                size="large"
+                format="YYYY/MM/DD"
+                allowClear
+                minDate={new Date()}
+                onChange={(date) => setPickupDate(handleDateFormat(date))}
+              />
+              <DatePicker
+                placeholder="Return Date"
+                placement="topLeft"
+                size="large"
+                format="YYYY/MM/DD"
+                allowClear
+                minDate={new Date()}
+                onChange={(date) => setReturnDate(handleDateFormat(date))}
+              />
+              <select
+                className=""
+                style={{ color: 'rgb(255, 193, 7)' }}
+                name="Vehicle"
+                defaultValue=""
+                label="Select a Vehicle"
+                onChange={handleVehicleId}
+                required
+              >
+                <option value="" disabled>Select a Vehicle</option>
+                <option value={allVehicles.id} key={allVehicles.id}>
+                  {allVehicles.name}
+                </option>
+              </select>
+            </div>
+            <div className="mt-3">
+              <Button
+                type="button"
+                onClick={handleBooking}
+                variant="gradient"
+                className="text-uppercase font-weight-bold py-3 btn btn-primary w-100"
+                style={{ color: 'rgb(255, 193, 7)' }}
+              >
+                <span>Reserve</span>
+              </Button>
+            </div>
+          </div>
+        </>
+      )
   );
 };
 
