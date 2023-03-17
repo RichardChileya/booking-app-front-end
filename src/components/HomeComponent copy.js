21e66b52ed82d6d58f64d8c8fe226a843660c50e
RichardChileya <richardchileyawk@gmail.com>
2023-03-17 23:26:23 +0200

    Add carousel draft code

--- src/components/HomeComponent copy.js
+++ src/components/HomeComponent copy.js
@@ -1,12 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { vehicles, listVehicles } from '../redux/reducer/vehicles/vehicleSlice';
 import '../App.css';
 import VehiclesList from './VehiclesList';
 
 const HomeComponent = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  // End whe
   const carData = useSelector(vehicles);
   const dispatch = useDispatch();
 
@@ -16,15 +37,15 @@ const HomeComponent = () => {
   return (
     <>
       <h2 className="container">Please select a car </h2>
-      <div className="container">
-        <div className="car-info ">
-          <Carousel>
+      <Carousel responsive={responsive}>
+        <div className="container">
+          <div className="car-info ">
             {carData.map((car) => (
               <VehiclesList key={car.id} item={car} />
             ))}
-          </Carousel>
+          </div>
         </div>
-      </div>
+      </Carousel>
 
       {/* <div>Item 1</div>
       <div>Item 2</div>
--- src/components/VehiclesList.js
+++ src/components/VehiclesList.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
 const VehiclesList = ({ item }) => {
   const path = `/vehicle-details/${item.id}`;
   return (
-    <div className="car-details col-md-4 col-sm-6">
-      <Link to={path}>
-        <img src={item.image} alt=" vehicles" className="car-imgs" />
-        <h2 className="vehicle-name">{item.name}</h2>
-      </Link>
-      <p>{item.description}</p>
-      <div className="icons-container">
-        <span><img src="https://cdn-icons-png.flaticon.com/512/4628/4628653.png" alt="fb icon" className="icons" /></span>
-        <span><img src="https://cdn-icons-png.flaticon.com/512/356/356076.png" alt="twitter icon" className="icons" /></span>
-        <span><img src="https://cdn-icons-png.flaticon.com/512/1362/1362857.png" alt="instagram icon" className="icons" /></span>
-      </div>
+    <Carousel.Item>
 
-    </div>
+      <div className="car-details col-md-4 col-sm-6">
+        <Link to={path}>
+          <img src={item.image} alt=" vehicles" className="car-imgs" />
+          <h2 className="vehicle-name">{item.name}</h2>
+        </Link>
+        <p>{item.description}</p>
+        <div className="icons-container">
+          <span><img src="https://cdn-icons-png.flaticon.com/512/4628/4628653.png" alt="fb icon" className="icons" /></span>
+          <span><img src="https://cdn-icons-png.flaticon.com/512/356/356076.png" alt="twitter icon" className="icons" /></span>
+          <span><img src="https://cdn-icons-png.flaticon.com/512/1362/1362857.png" alt="instagram icon" className="icons" /></span>
+        </div>
+
+      </div>
+    </Carousel.Item>
   );
 };
 
