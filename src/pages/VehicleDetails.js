8cc4e5845a5852540498756612762b5f19219fbe
RichardChileya <richardchileyawk@gmail.com>
2023-03-17 02:43:50 +0200

    Add vehicle details

--- src/pages/VehicleDetails.js
+++ src/pages/VehicleDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { vehicle, vehicleDetails } from '../redux/reducer/vehicles/vehicleSlice';
 
 const VehicleDetails = () => {
@@ -20,14 +20,41 @@ const VehicleDetails = () => {
   return (
     loading ? 'Loading'
       : (
-        <div className="row d-flex justify">
-          <div className="col-md-8 mt-5">
-            <img src={setVehicle.image} alt="Vehicle" width="100%" />
+        <>
+          <div className="row d-flex justify">
+            <div className=" details">
+              <div className="img-div">
+                <img className="detail-img" src={setVehicle.image} alt="Vehicle" width="100%" />
+              </div>
+              <div className="col-md-4">
+                <h2>{setVehicle.name}</h2>
+                <h4 className="">
+                  {' '}
+                  Model:
+                  {' '}
+                  {setVehicle.model}
+                </h4>
+                <p className="">{setVehicle.description}</p>
+                <p>
+                  {' '}
+                  Availablilty:
+                  {' '}
+                  {setVehicle.available ? 'Available' : 'Not Available' }
+                </p>
+                <p className="">
+                  {' '}
+                  daily price $:
+                  {' '}
+
+                  {' '}
+                  {setVehicle.daily_price}
+                  {' '}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="col-md-4">
-            {setVehicle.name}
-          </div>
-        </div>
+          <button className="btn btn-dark" type="button"><Link to="/">Back</Link></button>
+        </>
       )
   );
 };
