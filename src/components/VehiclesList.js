574e4be5684a4dfabed8e95be3b0f91f796fbcb3
RichardChileya <richardchileyawk@gmail.com>
2023-03-17 02:54:54 +0200

    Fix linters errors

--- src/components/VehiclesList.js
+++ src/components/VehiclesList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-
 const VehiclesList = ({ item }) => {
   const path = `/vehicle-details/${item.id}`;
   return (
